c2e038a605c80d9bfa4e4e2efa6aad28d6605a6c
Jatau Aren <jatauaren@gmail.com>
2023-05-24 17:02:29 +0200

    blog post

--- frontend/components/articles.js
+++ frontend/components/articles.js
@@ -6,6 +6,8 @@ import Spinner from "./_child/spinner"
 import Error from "./_child/error"
 import { client } from "../lib/client"
 import imageUrlBuilder  from '@sanity/image-url'
+import {FaComment} from "react-icons/fa"
+
 
 export default function articles({post}) {
 
@@ -19,7 +21,12 @@ export default function articles({post}) {
 
   return (
     <section className="container mx-auto md:px-20 py-10">
-        <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
+        <div className="w-[21rem] md:w-[30rem] mx-auto">
+
+
+        <h1 className="font-bold text-4xl text-center">Our Blog</h1>
+        <p className="pt-4 mb-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam repellendus quo modi quis quaerat obcaecati qui</p>
+        </div>
 
         {/* grid columns */}
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
@@ -44,25 +51,28 @@ function Post( { postData } ){
 
 
     return (
-        <div className="item">
+        <div className="item border-[2px] border-[#D2D2D2] rounded-[15px] overflow-hidden">
             <div className="images">
                 <Link
-        href={`/posts/${slug.current}`}  ><img src={urlFor(mainImage).width(500).height(350).url() || "/"} alt={`${title}'s picture`} className="rounded" width={500} height={350} /></Link>
+        href={`/posts/${slug.current}`}  ><img src={urlFor(mainImage).width(393).height(219).url() || "/"} alt={`${title}'s picture`} className="rounded" width={500} height={350} /></Link>
             </div>
-            <div className="info flex justify-center flex-col ml-5 md:ml-0 py-4">
-                <div className="cat">
+            <div className="info flex justify-center flex-col px-8 md:ml-0 py-4 text-[#1A1A1A]">
+            <div className="title">
+                    <Link
+                href={`/posts/${slug.current}`} ><a className="text-2xl font-bold hover:text-gray-600">{title || "Title"}</a></Link>
+                </div>
                     {/* <Link href={`/posts/${_id}`}><a className="text-orange-600 hover:text-orange-800">{category || "Unknown"}</a></Link> */}
+                <div className="mt-5 flex justify-between "> 
+
                     <Link
-                href={`/posts/${slug.current}`} ><a className="text-gray-800 hover:text-[#fb1c48]">- {new Date(publishedAt).toDateString() || "Unknown"}</a></Link>
-                </div>
-                <div className="title">
-                    <Link
-                href={`/posts/${slug.current}`} ><a className="text-2xl font-bold text-[802ccc] hover:text-gray-600">{title || "Title"}</a></Link>
-                </div>
-                <p className="text-gray-500 py-3">
-                   
+                href={`/posts/${slug.current}`} ><a className=" hover:text-[#fb1c48]">- {new Date(publishedAt).toDateString() || "Unknown"}</a></Link>
+                <p className="">
+                   <div className="flex">
+                   <FaComment/>
+                   <small className="ml-1">5</small>
+                   </div>
                 </p>
-            
+                </div>
             </div>
         </div>
     )
